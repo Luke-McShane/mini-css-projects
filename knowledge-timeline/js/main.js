2e25b28a0b225bd5cdcec796d577690cab3a2314
Luke-McShane <lukejsmcshane@gmail.com>
2020-05-22 12:33:58 +1000

    Commented CSS and JS files. Mini-project complete

--- knowledge-timeline/js/main.js
+++ knowledge-timeline/js/main.js
@@ -1,5 +1,10 @@
+// Select every list item element
 const items = document.querySelectorAll('#timeline li');
 
+/* The rect is the result of taking the current element and calling the getBoundingClientRect()
+  function on it, which returns the size of the element and its current position relative to the viewport.
+  With this information we can check whether or not the element is fully contained within the viewport and,
+  if so, we return true to the 'run' function */
 const isInViewport = el => {
   const rect = el.getBoundingClientRect();
   return (
@@ -11,6 +16,12 @@ const isInViewport = el => {
   );
 };
 
+/* The run function.
+  This iterates through every list item element and, for each element, calls the
+  isInViewport function, which will return true or false depending on whether or not
+  the current list item element is fully contained within the viewport or not.
+  If the returned value is true, then the list item has the class 'show' added to it and
+  if false, has the class 'show' removed from it, if it exists for that item */
 const run = () =>
   items.forEach(item => {
     if (isInViewport(item)) {
@@ -21,7 +32,9 @@ const run = () =>
     }
   });
 
-// Events
+/* Events
+  When the window is loaded, resized, or when the user scrolls on the page, call the
+  'run' function */
 window.addEventListener('load', run);
 window.addEventListener('resize', run);
 window.addEventListener('scroll', run);
